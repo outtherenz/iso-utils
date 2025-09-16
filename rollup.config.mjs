e4a4a9fbd56825f7061b22d482e5b45f3ade46cd
Rajesh kumar Bolisetty <rajesh.bnv@gmail.com>
2025-09-16 15:10:00 +1200

    support both esm and common js

---
+++ rollup.config.mjs
@@ -0,0 +1,27 @@
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.esm.js",
+        format: "esm"
+      },
+      {
+        file: "dist/index.cjs",
+        format: "cjs"
+      }
+    ],
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      typescript({
+        tsconfig: "./tsconfig.json",
+        declaration: false,
+      })
+    ]
+  }
+];
